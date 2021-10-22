b5e1a2d76caf37279e3713fc4d85757ca12a2ece
Evyatar <29202833+Evyatar-Kraus@users.noreply.github.com>
2021-10-22 13:18:21 +0300

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 'use strict'
 import {getInitialWarmup, getInitialExercise, getInitialWorkoutPlan, getInitialUserSettings , getInitialState}  from './initial-states.js';
 import {getChosenWorkout} from './methods.js';
-import {DEFAULT_WARMUP_NAME} from './constants.js';
+import {createExercise, updateExercise, deleteExercise, getExercise} from './crud-functions.js';
+import {createWorkout, updateWorkout, deleteWorkout, getWorkout} from './crud-functions.js';
 import {renderExercise,renderExerciseList,renderWorkout,renderWorkoutList} from './render-functions.js';
-
 
 //get initialState  - from getInitialState or TODO local-storage
 const initialState =  getInitialState();
 //put state
-window.intervalAppState = state;
+window.intervalAppState = initialState;
 //render by state on load
 const workoutListEl = document.querySelector("#workout_list");
 
@@ -21,7 +21,23 @@ const submitBtn = document.querySelector("form");
 
 submitBtn.addEventListener('submit',(e)=>{
     e.preventDefault();
-    e.target.
+    console.log([...e.target.elements]);
+    let formValues = [...e.target.elements]
+        //give me only form inputs with non empty value - exclude button el and the like
+        .filter(
+         (el) => el.value != '')
+         //with reduce assemble an object with key:value pairs of the form values
+        .reduce(
+            (prev,curr) => {
+                prev[curr.name] =  curr.value;
+                return prev;
+            }
+            ,{}); //reduce initial starting value - an empty object - to contain all form values by inputname:inputvalue
+    console.log(formValues);
+    
+
+
+
 });
 
 //on btn save workout save the current exercises into a new workout and add it to workouts in the state
@@ -30,13 +46,13 @@ submitBtn.addEventListener('submit',(e)=>{
 
 //on btn click play workout
 
-const playButtonEl  = document.querySelector(".play-button");
-playButtonEl.addEventListener('click',
-    (event)=>{
-        //TODO get the
-        startWorkoutPlan(chosenWorkout);
-    }
-);
+// const playButtonEl  = document.querySelector(".play-button");
+// playButtonEl.addEventListener('click',
+//     (event)=>{
+//         //TODO get the
+//         // startWorkoutPlan(chosenWorkout);
+//     }
+// );
 
 
 
